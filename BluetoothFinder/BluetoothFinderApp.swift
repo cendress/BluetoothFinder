682f87520247eaad339d87048ecf288add36a272
cendress <95492688+cendress@users.noreply.github.com>
2024-07-27 06:11:21 -0400

    Initial Commit

---
+++ BluetoothFinder/BluetoothFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothFinderApp.swift
+//  BluetoothFinder
+//
+//  Created by Christopher Endress on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
