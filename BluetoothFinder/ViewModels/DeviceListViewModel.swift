b9af1016a4946a4387986d0a3582dabb7d7bf294
cendress <95492688+cendress@users.noreply.github.com>
2024-07-27 08:07:18 -0400

    Add DeviceListViewModel to watch for changes of bluetooth devices

---
+++ BluetoothFinder/ViewModels/DeviceListViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  DeviceListViewModel.swift
+//  BluetoothFinder
+//
+//  Created by Christopher Endress on 7/27/24.
+//
+
+import Combine
+import Foundation
+
+class DeviceListViewModel: ObservableObject {
+    @Published var devices: [BluetoothDevice] = []
+    
+    // Add functions for bluetooth scanning
+}
