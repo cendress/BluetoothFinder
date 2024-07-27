a907943f3b1661ab79227770361fb39d796f0053
cendress <95492688+cendress@users.noreply.github.com>
2024-07-27 07:58:13 -0400

    Create BluetoothDevice data model

---
+++ BluetoothFinder/Models/BluetoothDevice.swift
@@ -0,0 +1,15 @@
+//
+//  BluetoothDevice.swift
+//  BluetoothFinder
+//
+//  Created by Christopher Endress on 7/27/24.
+//
+
+import Foundation
+
+struct BluetoothDevice: Identifiable {
+    // Use the device's name as the unique identifier
+    var id: String { name }
+    let name: String
+    let rssi: Int
+}
