a017dcff7be5f487bc49d73d4352bcf4834dae07
cendress <95492688+cendress@users.noreply.github.com>
2024-07-27 08:13:31 -0400

    Create a device list view that lists all Bluetooth devices

---
+++ BluetoothFinder/Views/DeviceListView.swift
@@ -0,0 +1,29 @@
+//
+//  DeviceListView.swift
+//  BluetoothFinder
+//
+//  Created by Christopher Endress on 7/27/24.
+//
+
+import SwiftUI
+
+struct DeviceListView: View {
+    @ObservedObject var viewModel: DeviceListViewModel
+    
+    var body: some View {
+        NavigationStack {
+            List(viewModel.devices) { device in
+                HStack {
+                    Text(device.name)
+                    Spacer()
+                    Text("RSSI: \(device.rssi)")
+                }
+            }
+            .navigationTitle("Nearby Devices")
+        }
+    }
+}
+
+#Preview {
+    DeviceListView(viewModel: DeviceListViewModel())
+}
