08e2eab292aff4abae17fbc3367454217165b7a0
cendress <95492688+cendress@users.noreply.github.com>
2024-07-28 10:08:30 -0400

    Convert model back to a struct

--- BluetoothFinder/Models/BluetoothDevice.swift
+++ BluetoothFinder/Models/BluetoothDevice.swift
@@ -7,18 +7,11 @@
 
 import Foundation
 
-@Observable
-class BluetoothDevice: Identifiable {
+struct BluetoothDevice: Identifiable {
     // Use the device's name as the unique identifier
     var id: String { name }
     let name: String
     var rssi: Int
     // TX power might not be available for all devices
     let txPower: Int?
-    
-    init(name: String, rssi: Int, txPower: Int?) {
-        self.name = name
-        self.rssi = rssi
-        self.txPower = txPower
-    }
 }
