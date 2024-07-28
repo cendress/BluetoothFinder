71f549fbb61a2207cde49442203740fe1e8d08bf
cendress <95492688+cendress@users.noreply.github.com>
2024-07-28 07:44:14 -0400

    Add infinite duration for haptics

--- BluetoothFinder/Views/DeviceLocationScreen/DeviceLocationView.swift
+++ BluetoothFinder/Views/DeviceLocationScreen/DeviceLocationView.swift
@@ -9,9 +9,6 @@ import CoreHaptics
 import SwiftUI
 
 struct DeviceLocationView: View {
-    
-    //MARK: - Properties
-    
     @Environment(\.dismiss) var dismiss
     @State private var engine: CHHapticEngine?
     
@@ -20,8 +17,6 @@ struct DeviceLocationView: View {
     private var backgroundColor: Color {
         device.rssi > -60 ? .green : .red
     }
-    
-    //MARK: - View body
     
     var body: some View {
         GeometryReader { geometry in
@@ -40,10 +35,7 @@ struct DeviceLocationView: View {
                 DistanceInformationView(device: device)
             }
             .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
-            .onAppear(perform: prepareHaptics)
-            .onChange(of: device.rssi) {
-                performHapticFeedback()
-            }
+            .onAppear(perform: performHapticFeedback)
         }
         .background(backgroundColor)
         .navigationTitle("Device Location")
@@ -61,45 +53,45 @@ struct DeviceLocationView: View {
         }
     }
     
-    //MARK: - Circle size method
-    
     private func circleSize(geometry: GeometryProxy) -> CGFloat {
         let normalizedRSSI = min(max(1.0 - (Double(device.rssi + 100) / 70.0), 0.2), 1.0)
         return normalizedRSSI * geometry.size.width
     }
     
-    //MARK: - Haptics
-    
     private func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
         do {
             self.engine = try CHHapticEngine()
             try engine?.start()
         } catch {
-            print("Error starting the haptic engine: \(error)")
+            print("There was an error creating or starting the haptic engine: \(error.localizedDescription)")
         }
     }
-    
+
     private func performHapticFeedback() {
-        guard let engine = engine else { return }
-        
+        prepareHaptics()
+
+        guard let engine = self.engine, CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            print("Haptic feedback is not supported on this device.")
+            return
+        }
+
         let normalizedRSSI = 1 - min(max(Double(device.rssi) / -100, 0), 1)
         let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(normalizedRSSI))
         let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(normalizedRSSI))
-        let event = CHHapticEvent(eventType: .hapticContinuous, parameters: [intensity, sharpness], relativeTime: 0)
-        
+        let event = CHHapticEvent(eventType: .hapticContinuous, parameters: [intensity, sharpness], relativeTime: 0, duration: .infinity)
+
         do {
             let pattern = try CHHapticPattern(events: [event], parameters: [])
             let player = try engine.makePlayer(with: pattern)
             try player.start(atTime: 0)
         } catch {
-            print("Error playing haptic pattern: \(error)")
+            print("Error playing haptic pattern: \(error.localizedDescription)")
         }
     }
 }
 
 #Preview {
     let sampleDevice = BluetoothDevice(name: "Router", rssi: 0, txPower: nil)
+    
     return DeviceLocationView(device: sampleDevice)
 }
