2ae30927aa2a948c31457cd70856d807731c179f
cendress <95492688+cendress@users.noreply.github.com>
2024-07-28 10:00:34 -0400

    Implement more visual changes to the circle to indicate when a device is close

--- BluetoothFinder/Views/DeviceLocationScreen/ProximityCircleView.swift
+++ BluetoothFinder/Views/DeviceLocationScreen/ProximityCircleView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct ProximityCircleView: View {
     var rssi: Int
     var geometry: GeometryProxy
-
+    
     private let closeProximityThreshold = -40
     
     var body: some View {
         Circle()
             .fill(self.isClose ? Color.white : Color.white.opacity(0.8))
+            .overlay(
+                Circle()
+                    .stroke(Color.black, lineWidth: isClose ? 4 : 0)
+                    .animation(.easeInOut(duration: 0.5).repeatForever(autoreverses: true), value: isClose)
+                    .opacity(isClose ? 1 : 0)
+            )
             .animation(.easeInOut(duration: 0.5), value: isClose)
             .shadow(radius: 10)
             .padding()
