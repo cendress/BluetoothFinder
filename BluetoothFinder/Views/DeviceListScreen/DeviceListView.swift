e84f86923de98841921ceba8f0a3a59430ef147d
cendress <95492688+cendress@users.noreply.github.com>
2024-07-28 06:36:34 -0400

    Refactor DeviceListView to include separated UI components

---
+++ BluetoothFinder/Views/DeviceListScreen/DeviceListView.swift
@@ -0,0 +1,32 @@
+//
+//  DeviceListView.swift
+//  BluetoothFinder
+//
+//  Created by Christopher Endress on 7/27/24.
+//
+
+import SwiftUI
+
+struct DeviceListView: View {
+    @ObservedObject var viewModel: DeviceListViewModel
+    
+    var body: some View {
+        Group {
+            if viewModel.devices.isEmpty {
+                EmptyDeviceView()
+            } else {
+                DeviceList(devices: viewModel.devices)
+            }
+        }
+        .toolbar {
+            Button("Scan", systemImage: "antenna.radiowaves.left.and.right") {
+                viewModel.startScanning()
+            }
+        }
+        .navigationTitle("Nearby Devices")
+    }
+}
+
+#Preview {
+    DeviceListView(viewModel: DeviceListViewModel())
+}
